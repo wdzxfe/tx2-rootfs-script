295dfaddf03da1003519d5a8a63ec58a2b848f1a
Zheng Wang <271568700@qq.com>
2018-04-10 22:31:40 +0800

    modification: rootfs and network

--- build-step1.sh
+++ build-step1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get -y install qemu-user-static debootstrap
-mkdir ./rootfs
+mkdir ../rootfs
 export ARCH=arm64
-PACKAGE=nano
+PACKAGE=vim
 RELEASE=xenial
 
 debootstrap \
@@ -13,10 +13,10 @@ debootstrap \
         --variant=minbase \
         --include=$PACKAGE \
         $RELEASE \
-        ./rootfs
-cp /usr/bin/qemu-aarch64-static ./rootfs/usr/bin
-cp ./build-step2.sh ./rootfs/
-cd ./rootfs
+        ../rootfs
+cp /usr/bin/qemu-aarch64-static ../rootfs/usr/bin
+cp ./build-step2.sh ../rootfs/
+cd ../rootfs
 chroot . /bin/bash -c "/debootstrap/debootstrap --second-stage" 
 # Open a QEMU shell to make any additional modifications. You can use apt-get at this point.
 chroot . /bin/bash
@@ -25,4 +25,4 @@ rm ./usr/bin/qemu-aarch64-static
 # When done create a filesystem archive
 # sudo tar -cvjSf Tegra_Linux_Sample-Root-Filesystem_${RELEASE}_${PACKAGE}_aarch64.tbz2 *
 cd -
-du -sh ./rootfs
+du -sh ../rootfs
--- build-step2.sh
+++ build-step2.sh
@@ -37,7 +37,7 @@ echo "iface eth0 inet static" >> /etc/network/interfaces
 echo "address 192.168.3.55" >> /etc/network/interfaces
 echo "netmask 255.255.255.0" >> /etc/network/interfaces
 # echo "network 192.168.3.1" >> /etc/network/interfaces
-echo "gateway 192.168.3.0" >> /etc/network/interfaces
+echo "gateway 192.168.3.1" >> /etc/network/interfaces
 echo "dns-nameservers 8.8.8.8 8.8.4.4" >> /etc/network/interfaces
 
 #clean
